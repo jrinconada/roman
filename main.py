8ef9a2b13e75d4dc500b08d68405576d62cd912a
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-06-08 10:41:35 +0200

    Few changes for numbers 1 to 10

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ TO_DECIMAL = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 # Input
 # number = eval(input('Give me a number: '))
-number = 5
+number = 8
 
 one = TO_ROMAN[1]
-five = TO_ROMAN[5]
+limit = TO_ROMAN[5]
 result = ''
 repeated = 0
 
 for i in range(number):
-    if repeated < 3:
+    if repeated < 3:  # I, II, III and VI, VII, VIII
         result = result + one
         repeated += 1
-    elif repeated == 3:
-        result = one + five
+    elif repeated == 3:  # IV and IX
+        result = one + limit
         repeated += 1
-    else:
-        result = five
+    elif repeated == 4:  # V and X
+        result = limit
+        limit = TO_ROMAN[10]
         repeated = 0
 
 # Output
