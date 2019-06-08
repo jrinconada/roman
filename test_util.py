8bc0266b63be939d194f9dd7c6dc5c0cb42efedc
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-06-08 13:52:18 +0200

    Tried different methods of getting the nth digit of a number

--- test_util.py
+++ test_util.py
@@ -1,10 +1,24 @@
 import unittest
 from util import get_digit
+from util import get_digit_b
+from util import get_digit_c
 
 
-class TestGetDigit(unittest.TestCase):
+class TestUtil(unittest.TestCase):
     def test_get_digit(self):
         self.assertEqual(get_digit(5438, 1), 8)
         self.assertEqual(get_digit(5438, 2), 3)
         self.assertEqual(get_digit(5438, 3), 4)
         self.assertEqual(get_digit(5438, 4), 5)
+
+    def test_get_digit_b(self):
+        self.assertEqual(get_digit_b(5438, 1), 8)
+        self.assertEqual(get_digit_b(5438, 2), 3)
+        self.assertEqual(get_digit_b(5438, 3), 4)
+        self.assertEqual(get_digit_b(5438, 4), 5)
+
+    def test_get_digit_c(self):
+        self.assertEqual(get_digit_c(5438, 1), 8)
+        self.assertEqual(get_digit_c(5438, 2), 3)
+        self.assertEqual(get_digit_c(5438, 3), 4)
+        self.assertEqual(get_digit_c(5438, 4), 5)
--- util.py
+++ util.py
@@ -1,7 +1,15 @@
 
 
 def get_digit(number, digit):
-    sum = 0
+    digit_sum = 0
     for i in range(digit):
-        sum += number - ((number // 10**(i+1)) * 10**(i+1) + sum)
-    return sum // 10**(digit-1)
+        digit_sum += number - ((number // 10**(i+1)) * 10**(i+1) + digit_sum)
+    return digit_sum // 10**(digit-1)
+
+
+def get_digit_b(number, digit):
+    return number % 10**digit // 10**(digit-1)
+
+
+def get_digit_c(number, digit):
+    return int(str(number)[-digit])
