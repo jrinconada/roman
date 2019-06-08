b65af2582a7944d616c93589b8925e845c838a11
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-06-08 10:51:28 +0200

    Code structured in a translator module

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
+import translator
 
-TO_ROMAN = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-TO_DECIMAL = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 # Input
 # number = eval(input('Give me a number: '))
 number = 8
 
-one = TO_ROMAN[1]
-limit = TO_ROMAN[5]
-result = ''
-repeated = 0
-
-for i in range(number):
-    if repeated < 3:  # I, II, III and VI, VII, VIII
-        result = result + one
-        repeated += 1
-    elif repeated == 3:  # IV and IX
-        result = one + limit
-        repeated += 1
-    elif repeated == 4:  # V and X
-        result = limit
-        limit = TO_ROMAN[10]
-        repeated = 0
+result = translator.translate(number)
 
 # Output
 print(result)
---
+++ translator.py
@@ -0,0 +1,24 @@
+
+TO_ROMAN = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+TO_DECIMAL = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+
+def translate(number):
+    one = TO_ROMAN[1]
+    limit = TO_ROMAN[5]
+    result = ''
+    repeated = 0
+
+    for i in range(number):
+        if repeated < 3:  # I, II, III and VI, VII, VIII
+            result = result + one
+            repeated += 1
+        elif repeated == 3:  # IV and IX
+            result = one + limit
+            repeated += 1
+        elif repeated == 4:  # V and X
+            result = limit
+            limit = TO_ROMAN[10]
+            repeated = 0
+
+    return result
