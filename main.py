3941c5c09728d1e9ad974a931579aecd0a7920b8
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-06-09 23:39:31 +0200

    Added roman numerals validation

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from translator_b import translate_to_roman
 from translator_b import translate_to_decimal
 from translator_a import show
-from validation import valid
+from validation import valid_number
+from validation import valid_word
+from validation import valid_letters
 
 
 # Input
 number = input('Give me a number (roman or decimal): ')
 if number.isdigit():  # Decimal number to roman numeral
-    if not valid(eval(number)):
+    if not valid_number(eval(number)):
         result = "It must be an integer number between 1 and 3999"
     else:
         result = translate_to_roman(eval(number))
-else:  # Roman numeral to decimal number
+elif not valid_word(number):  # Roman numeral to decimal number
+    result = "Invalid letter, must be one of these: " + valid_letters()
+else:
     result = translate_to_decimal(number.upper())
 
 
--- validation.py
+++ validation.py
@@ -1,6 +1,23 @@
 
+VALID_LETTERS = ('I', 'V', 'X', 'L', 'C', 'D', 'M')
 
-def valid(user_input):
+def valid_number(user_input):
     """ Returns True if input is an integer number between 1 and 3999 """
     return 1 <= user_input <= 3999
 
+
+def valid_word(user_input=''):
+    """ Returns True if all letters are valid roman numerals """
+    user_input = user_input.upper()
+    for letter in VALID_LETTERS:  # Removes all the roman numerals letters
+        user_input = user_input.replace(letter, '')
+    return not user_input  # If user input is empty all the letters were roman numerals
+
+
+def valid_letters():
+    """ Returns a human readable list for the valid roman numerals """
+    valids = ''
+    for letter in VALID_LETTERS[:-2]:
+        valids += letter + ', '
+    valids += VALID_LETTERS[-2] + ' or ' + VALID_LETTERS[-1]
+    return valids
